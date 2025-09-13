add75a05c3060ede7a76cd6d3b6dddc2242aabf3
kimleng01 <lengk0274@gmail.com>
2025-09-13 23:09:54 +0700

    change code

--- src/components/animated-scroll.tsx
+++ src/components/animated-scroll.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { Button } from './ui/button';
+// import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
 // import { link } from 'fs';
 
