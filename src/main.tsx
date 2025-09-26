67f4e35cbc998269eff06d17db7f457db739ed55
kimleng01 <lengk0274@gmail.com>
2025-09-26 19:32:30 +0700

    Add portfolio with CV download

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import AllRoutes from './Routes/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolio'>
     <AllRoutes />
   </BrowserRouter>,
 )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -29,7 +29,9 @@ export const Home = () => {
                   }
               `}</style>
           <div className="relative inline-block p-0.5 rounded-full overflow-hidden hover:scale-105 transition duration-300 active:scale-100 before:content-[''] before:absolute before:inset-0 before:bg-[conic-gradient(from_0deg,_#00F5FF,_#00F5FF30,_#00F5FF)] button-wrapper">
-            <a download href="/Hean-kimleng.pdf">
+            <a 
+               download href="/public/Hean kimleng_CV.pdf"
+            >
               <button className="relative z-10 bg-gray-800 text-white rounded-full px-8 py-3 font-medium text-sm">
                 Download CV
               </button>
